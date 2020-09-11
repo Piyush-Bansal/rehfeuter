bd0b290dede7c83bc8471b1705506f62e64fd855
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2020-09-11 13:01:22 +0530

    scroll script removed

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // smooth scroll
-<script src="https://min30327.github.io/luxy.js/dist/js/luxy.js"></script>
-
 <script charset="utf-8">  
  var isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
  if (!isMobile) {
